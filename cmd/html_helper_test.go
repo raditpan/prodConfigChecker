b548bdb0698bd7d882113c955929211c80a4b330
Radit Panjapiyakul <radit.pan@ascendcorp.com>
2021-04-24 16:33:09 +0700

    Add more test cases in html helper

--- cmd/html_helper_test.go
+++ cmd/html_helper_test.go
@@ -13,10 +13,13 @@ func Test_DiffPrettyHtmlLeft(t *testing.T) {
 		text1	string
 		text2	string
 		expected	string
+		fixTab	bool
 	}{
-		{"Delete diff at the end", "test1", "test2", "<span>test</span><del style=\"background:#ffb5b5;\">1</del>"},
-		{"Delete diff at the start", "yestest", "test", "<del style=\"background:#ffb5b5;\">yes</del><span>test</span>"},
-		{"Delete diff in the middle", "test123text", "testtext", "<span>test</span><del style=\"background:#ffb5b5;\">123</del><span>text</span>"},
+		{"Delete diff at the end", "test1", "test2", "<span>test</span><del style=\"background:#ffb5b5;\">1</del>", false},
+		{"Delete diff at the start", "yestest", "test", "<del style=\"background:#ffb5b5;\">yes</del><span>test</span>", false},
+		{"Delete diff in the middle", "test123text", "testtext", "<span>test</span><del style=\"background:#ffb5b5;\">123</del><span>text</span>", false},
+		{"Fix space and tabs", " test1", " test2", "<span>&nbsp;test</span><del style=\"background:#ffb5b5;\">1</del>", true},
+		{"Replace newline", "test\n1", "test\n2", "<span>test<br></span><del style=\"background:#ffb5b5;\">1</del>", true},
 	}
 
 	for _, tt := range tests {
@@ -24,7 +27,7 @@ func Test_DiffPrettyHtmlLeft(t *testing.T) {
 			dmp := diffmatchpatch.New()
 			diffs := dmp.DiffMain(tt.text1, tt.text2, false)
 
-			result := DiffPrettyHtmlLeft(diffs, false)
+			result := DiffPrettyHtmlLeft(diffs, tt.fixTab)
 
 			if len(diffs) == 1 {
 				t.Errorf("Result html diff has no diff")
@@ -44,10 +47,13 @@ func Test_DiffPrettyHtmlRight(t *testing.T) {
 		text1	string
 		text2	string
 		expected	string
+		fixTab	bool
 	}{
-		{"Insert diff at the end", "test1", "test2", "<span>test</span><span style=\"background:#d1ffd1;\">2</span>"},
-		{"Insert diff at the start", "test", "yestest", "<span style=\"background:#d1ffd1;\">yes</span><span>test</span>"},
-		{"Insert diff in the middle", "testtext", "test123text", "<span>test</span><span style=\"background:#d1ffd1;\">123</span><span>text</span>"},
+		{"Insert diff at the end", "test1", "test2", "<span>test</span><span style=\"background:#d1ffd1;\">2</span>", false},
+		{"Insert diff at the start", "test", "yestest", "<span style=\"background:#d1ffd1;\">yes</span><span>test</span>", false},
+		{"Insert diff in the middle", "testtext", "test123text", "<span>test</span><span style=\"background:#d1ffd1;\">123</span><span>text</span>", false},
+		{"Fix space and tabs", " test1", " test2", "<span>&nbsp;test</span><span style=\"background:#d1ffd1;\">2</span>", true},
+		{"Replace newline", "test\n1", "test\n2", "<span>test<br></span><span style=\"background:#d1ffd1;\">2</span>", true},
 	}
 
 	for _, tt := range tests {
@@ -55,7 +61,7 @@ func Test_DiffPrettyHtmlRight(t *testing.T) {
 			dmp := diffmatchpatch.New()
 			diffs := dmp.DiffMain(tt.text1, tt.text2, false)
 
-			result := DiffPrettyHtmlRight(diffs, false)
+			result := DiffPrettyHtmlRight(diffs, tt.fixTab)
 
 			if len(diffs) == 1 {
 				t.Errorf("Result html diff has no diff")
